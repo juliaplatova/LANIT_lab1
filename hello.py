cce33faf77379b7ba7a339d50664ba210d6a1d6c
Julia Platova <j.a.platova@gmail.com>
2026-04-24 17:46:04 +0300

    lab1: ask name in hello script

--- hello.py
+++ hello.py
@@ -13,13 +13,16 @@ def _join_mess(parts):
 def main():
 	words = ["Hello", "appsec", "world"]
 	message = _join_mess(words)
+	name = input("Enter your name: ").strip()
+	if name == "":
+		name = "unknown"
 
 	if len(_sys.argv) > 1:
 		message = message[:]
 	else:
 		message = "".join([c for c in message])
 
-	print(message)
+	print(message + " from @" + name)
 
 
 if __name__ == "__main__":
