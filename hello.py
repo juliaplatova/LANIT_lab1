9979038aa93c0d642eddbc8dc744938d20b8676b
juliaplatova <j.a.platova@gmail.com>
2026-04-24 18:13:22 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,10 +10,10 @@ def main(
     Говорит "Привет" пользователю, опционально используя фамилию и формальный стиль.
     Added to comment on [master]
     """
-    if formal:
-        print(f"Добрый день, {name} {lastname}!")
-    else:
-        print(f"Привет, {name}!")
+    if formal: # Added to comment on [master]
+        print(f"Добрый день, {name} {lastname}!") # Added to comment on [master]
+    else: # Added to comment on [master]
+        print(f"Привет, {name}!") # Added to comment on [master]
 
 
 if __name__ == "__main__":
